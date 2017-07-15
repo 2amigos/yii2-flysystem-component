27a7a5c94d692dbdb508bc9b969c67f677fa4e82
Antonio Ramirez <amigo.cobos@gmail.com>
2017-07-15 08:41:19 +0200

    update phpdoc

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,11 +1,11 @@
 <?php
- /**
- * 
- * bootstrap.php
+/**
+ * This file is part of the 2amigos/yii2-flysystem-component project.
  *
- * @author Antonio Ramirez <amigo.cobos@gmail.com>
- * @link http://www.ramirezcobos.com/
- * @link http://www.2amigos.us/
+ * (c) 2amigOS! <http://2amigos.us/>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
  */
 
 defined('YII_DEBUG') or define('YII_DEBUG', true);
@@ -16,7 +16,9 @@ require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
 Yii::setAlias('@tests', __DIR__);
 
-new \yii\console\Application([
-    'id' => 'testApp',
-    'basePath' => __DIR__
-]);
+new \yii\console\Application(
+    [
+        'id' => 'testApp',
+        'basePath' => __DIR__
+    ]
+);
